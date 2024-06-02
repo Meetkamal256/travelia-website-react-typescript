6a3fbf7ae5c5800cf36ce0266442cd69c2da40ab
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-02 15:17:45 +0100

    preloader component styling done

--- src/components/preloader/Preloader.tsx
+++ src/components/preloader/Preloader.tsx
@@ -1,17 +1,11 @@
 import "./preloader.css";
-import preloaderImg from "../../assets/preloader.svg"
+import preloaderImg from "../../assets/preloader.svg";
 
 const Preloader = () => {
   return (
-    <div className="preloader" data-preloader>
+    <div className="preloader">
       <div className="preloader-inner">
-        <img
-          src={preloaderImg}
-          width="50"
-          height="50"
-          alt=""
-          className="img"
-        />
+        <img src={preloaderImg} width="50" height="50" alt="" className="img" />
       </div>
     </div>
   );
