85a0772d883ec1dd788281da055780a05cd13f00
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-02 17:37:05 +0100

    added the pageLinks array to data file

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,7 +1,32 @@
 import "./navbar.css";
-
+import Logo from "../../assets/logo.svg";
 const Header = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="container nav__container">
+        <div className="nav__logo">
+          <img src={Logo} alt="" />
+        </div>
+        <ul className="nav__items">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
---
+++ src/data.tsx
@@ -0,0 +1,27 @@
+export const pageLinks = [
+  {
+    id: 1,
+    path: "/",
+    text: "Home",
+  },
+  {
+    id: 2,
+    path: "#about",
+    text: "About",
+  },
+  {
+    id: 3,
+    path: "#services",
+    text: "Services",
+  },
+  {
+    id: 4,
+    path: "#pricing",
+    text: "Pricing",
+  },
+  {
+    id: 5,
+    path: "#contact",
+    text: "#Contact",
+  },
+];
