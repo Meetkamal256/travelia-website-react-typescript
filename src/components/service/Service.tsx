1df688e4d7147cb51ce8f99d4179a6a313af2a0a
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-05 22:23:49 +0100

    used map method to fetch serviceList array from data file

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import './service.css';
+import "./service.css";
+import { serviceList } from "../../data";
 
 const Service = () => {
   return (
-    <div>Service</div>
-  )
-}
+    <section id="service">
+      <div className="container service__container">
+        <div className="title__wrapper">
+          <p className="section__subtitle">What we Serve</p>
+          <h2 className="title section__title">Top Values For You</h2>
+          <p className="section__text">
+            Try a variety of benefits when using our services.
+          </p>
+        </div>
+        {serviceList.map((service) => {
+          const { id, icon, title, text } = service;
+          return (
+            <div className="service__card" key={id}>
+              <div className="card__icon">
+                <img src={icon} alt="" />
+              </div>
+              <h3 className="title card__title">{title}</h3>
+              <p className="card__text">{text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Service
+export default Service;
--- src/data.tsx
+++ src/data.tsx
@@ -1,3 +1,7 @@
+import icon1 from "./assets/service-icon-1.svg";
+import icon2 from "./assets/service-icon-2.svg";
+import icon3 from "./assets/service-icon-3.svg";
+
 export const pageLinks = [
   {
     id: 1,
@@ -23,5 +27,26 @@ export const pageLinks = [
     id: 5,
     path: "#contact",
     text: "#Contact",
+  },
+];
+
+export const serviceList = [
+  {
+    id: 1,
+    icon: icon1,
+    title: "Lots Of Choices",
+    text: "Total 460+ destinations that we work with.",
+  },
+  {
+    id: 2,
+    icon: icon2,
+    title: "Best Tour Guide",
+    text: "Our tour guide with 15+ years of experience.",
+  },
+  {
+    id: 3,
+    icon: icon3,
+    title: "Easy Booking",
+    text: " With an easy and fast ticket purchase process.",
   },
 ];
