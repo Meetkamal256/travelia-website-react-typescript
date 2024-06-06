8f233c51397ff4ec434aaa1169c98a8c3533d6d0
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-06 21:33:24 +0100

    modified the destinationList array in data file

--- src/data.tsx
+++ src/data.tsx
@@ -3,6 +3,11 @@ import icon2 from "./assets/service-icon-2.svg";
 import icon3 from "./assets/service-icon-3.svg";
 
 import image1 from "./assets/destination-1.jpg";
+import image2 from "./assets/destination-2.jpg";
+import image3 from "./assets/destination-3.jpg";
+import image4 from "./assets/destination-4.jpg";
+import image5 from "./assets/destination-5.jpg";
+import image6 from "./assets/destination-6.jpg";
 
 export const pageLinks = [
   {
@@ -62,5 +67,50 @@ export const destinationList = [
     address: "Rome, Italy",
     rating: 4.7,
     review: "(11.1k Review)",
+  },
+  {
+    id: 2,
+    image: image2,
+    price: "$500",
+    title: "Enjoy the Beauty of the Pakistan Monument",
+    address: "Islamabad, Pakistan",
+    rating: 4.6,
+    review: "(16.9k Review)",
+  },
+  {
+    id: 3,
+    image: image3,
+    price: "$750",
+    title: "Enjoy the Beauty of the Burj Khalifa",
+    address: "Skyscraper in Dubai",
+    rating: 4.7,
+    review: "(120k Review)",
+  },
+  {
+    id: 4,
+    image: image4,
+    price: "$950",
+    title: "Enjoy the Beauty of the Milaidhoo",
+    address: "Island in the Maldives",
+    rating: 4.7,
+    review: "(70k Review)",
+  },
+  {
+    id: 5,
+    image: image5,
+    price: "$850",
+    title: "Enjoy the Beauty of the Ko Tao",
+    address: "Island in the Gulf of Thailand",
+    rating: 4.5,
+    review: "(985 Review)",
+  },
+  {
+    id: 6,
+    image: image6,
+    price: "$1000",
+    title: "Enjoy the Beauty of the Grand Mecidiye Mosque",
+    address: "Mosque in Istanbul, Türkiye",
+    rating: 4.8,
+    review: "(29.6k Review)",
   },
 ];
