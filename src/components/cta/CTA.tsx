8ed7123b98198746628e21eeac27ef85caa43727
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-07 23:01:03 +0100

    cta component jsx and styling done

--- src/components/cta/CTA.tsx
+++ src/components/cta/CTA.tsx
@@ -1,8 +1,31 @@
-import React from "react";
 import "./cta.css";
+import worldIcon from "../../assets/title-icon.svg";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section id="cta">
+      <div className="container cta__container">
+        <div className="cta__card">
+          <h2 className="section__title title">
+            Prepare Yourself & Let’s Explore The Beauty Of The World
+            <img
+              src={worldIcon}
+              alt="world icon"
+              width="36"
+              height="36"
+              loading="lazy"
+            />
+          </h2>
+          <p className="section__text">
+            We have many special offers especially for you.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Get Started
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CTA;
