4cbb747846ca1c359fd5b451a9b06ed435b05364
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-08 01:12:58 +0100

    footer component jsx done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,145 @@
-import React from "react";
 import "./footer.css";
+import footerLogo from "../../assets/logo.svg";
+import { BsInstagram } from "react-icons/bs";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { CiPaperplane } from "react-icons/ci";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container footer__container">
+        <div className="footer__1">
+          <a href="#" className="footer__logo">
+            <img src={footerLogo} alt="footer logo" />
+          </a>
+          <p className="footer__text">
+            We always make our customer happy by providing as many choices as
+            possible
+          </p>
+          <ul className="footer__socials">
+            <li>
+              <a href="#" className="social__link">
+                <BsInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="social__link">
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="social__link">
+                <FaTwitter />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__2">
+          <p className="footer__title">About</p>
+          <ul className="footer__list">
+            <li>
+              <a href="#" className="footer__link">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                News
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Menu
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__3">
+          <p className="footer__title">Company</p>
+          <ul className="footer__list">
+            <li>
+              <a href="#" className="footer__link">
+                Why Company
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Partner With Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                FAQ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__4">
+          <p className="footer__title">Support</p>
+          <ul className="footer__list">
+            <li>
+              <a href="#" className="footer__link">
+                Account
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Support Center
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Feedback
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer__link">
+                Accessibility
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__5">
+          <p className="footer__title">Get In Touch</p>
+          <p className="footer__text">
+            Question or feedback? We’d love to hear from you.
+          </p>
+          <form action="" className="input__wrapper">
+            <input
+              type="email"
+              name="email_address"
+              placeholder="Email Address"
+              autoComplete="off"
+              required
+              className="input__field"
+            />
+            <button type="submit" className="input__btn">
+              <CiPaperplane />
+            </button>
+          </form>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
